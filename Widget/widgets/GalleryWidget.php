22ae049ea8058bbdc3f5ec904fd653a386ee72ee
Laurent Comex <laurent.comex@sfr.fr>
2017-05-17 14:51:23 +0200

    lcomex

--- Widget/widgets/GalleryWidget.php
+++ Widget/widgets/GalleryWidget.php
@@ -4,33 +4,37 @@ namespace Widget\widgets;
 
 use Widget\widgets\Widget;
 use Widget\models\Gallery;
+use System\Templator;
 
 class GalleryWidget extends Widget
 {
-	public function __construct($params = array())
-	{
-		$this->type = 'gallery';
-	}
+    public function __construct($params = array())
+    {
+        $this->type = 'gallery';
+    }
 
-	public function getHtml()
-	{
-		$html = '';
+    public function getHtml()
+    {
+        $html = '';
 
-		$id = 0;
-		if (!is_array($this->params)) {
-			$id = (int)$params;
-		} else if (isset($params['id'])) {
-			$id = (int)$params['id'];
-		}
+        $id = 0;
+        if (!is_array($this->params)) {
+            $id = (int)$params;
+        } else if (isset($params['id'])) {
+            $id = (int)$params['id'];
+        }
 
-		if ($id != 0) {
-			$gallery = Gallery::findById($id);
-			$viewFile = $this->getViewFile();
-			ob_start();
-			require $viewFile;
-			$html = ob_get_clean();
-		}
+        if ($id != 0) {
+            $gallery = Gallery::findById($id);
+            $viewFile = $this->getViewFile();
+            ob_start();
+            require $viewFile;
+            $html = ob_get_clean();
 
-		return $html;
-	}
+            $templator = new Templator();
+            $html = $templator->parse($html, array('gallery' => $gallery));
+        }
+
+        return $html;
+    }
 }
