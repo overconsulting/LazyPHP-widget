7a2c7cebd2de50fd0fdced34190f71c097e2a3b0
Laurent <laurent.Comex@sfr.fr>
2017-12-11 15:32:49 +0100

    lcomex

--- Widget/assets/js/gallery.js
+++ Widget/assets/js/gallery.js
@@ -1,3 +1,7 @@
+var galleryPopup = null;
+var galleryMedias = [];
+var galleryCurrentMedia = 0;
+
 function galleryMediasAdded()
 {
     var submitButton = document.getElementById("submit");
@@ -6,5 +10,106 @@ function galleryMediasAdded()
 }
 
 $(document).ready(function() {
+    $(".widget-gallery .gallery-media").on("click", galleryShowPopup);
 });
 
+function galleryShowPopup(event)
+{
+    var media = event.currentTarget;
+
+    galleryCurrentMedia = $(media).data("index");
+
+    var $gallery = $(media).parents(".widget-gallery");
+    galleryMedias = [];
+    $gallery.find(".gallery-media").each(function(index, element) {
+        var data = JSON.parse(decodeURIComponent($(element).data("media")));
+        galleryMedias.push(data);
+    });
+
+    params = {
+        postData: null,
+        id: "gallery_popup",
+        title: "",
+        url: "",
+        actions: {
+            load: galleryPopupLoadEvent.bind(this)
+        }
+    };
+
+    if (galleryPopup != null) {
+        galleryPopup.close();
+    }
+
+    galleryPopup = new LazyDialog();
+    galleryPopup.open(params);
+}
+
+function galleryPopupResizeEvent(event)
+{
+    var maxWidth = $("#gallery_popup .lazy-dialog-body").width();
+
+    var maxHeight =
+        $("#gallery_popup .lazy-dialog-body").height()/* -
+        $("#gallery_popup .gallery-popup-media-title").outerHeight() -
+        $("#gallery_popup .gallery-popup-media-description").outerHeight()*/;
+
+    $("#gallery_popup .gallery-popup-media").outerWidth(maxWidth);
+    $("#gallery_popup .gallery-popup-media").outerHeight(maxHeight);
+}
+
+function galleryPopupLoadEvent(event)
+{
+    $(window).on("resize", galleryPopupResizeEvent);
+
+    var active = "";
+    var orientation = "";
+    var html = "";
+    var i = 0;
+
+    for (i = 0; i < galleryMedias.length; i = i + 1) {
+        active = i == galleryCurrentMedia ? " active" : "";
+
+        if (galleryMedias[i].imageInfos.width > galleryMedias[i].imageInfos.height) {
+            orientation = " landscape";
+        } else {
+            orientation = " portrait";
+        }
+
+        html = html +
+            '<div class="gallery-popup-media'+active+'" data-index="'+i+'">' +
+                '<img class="gallery-popup-media-image'+orientation+'" src="' + galleryMedias[i].image + '" alt="" />' +
+                // '<div class="gallery-popup-media-title">'+galleryMedias[i].title+'</div>' +
+                // '<div class="gallery-popup-media-description">'+galleryMedias[i].description+'</div>' +
+            '</div>';
+    }
+
+    html = html + '<div class="gallery-popup-arrow gallery-popup-arrow-prev"><i class="fa fa-arrow-left"></i></div>';
+    html = html + '<div class="gallery-popup-arrow gallery-popup-arrow-next"><i class="fa fa-arrow-right"></i></div>';
+
+    var body = $("#gallery_popup .lazy-dialog-body")[0];
+    body.innerHTML = html;
+
+    $("#gallery_popup .gallery-popup-arrow").on("click", galleryPopupChangeMedia);
+
+    galleryPopupResizeEvent(null);
+}
+
+function galleryPopupChangeMedia(event)
+{
+    var arrow = event.currentTarget;
+
+    if ($(arrow).hasClass("gallery-popup-arrow-next")) {
+        galleryCurrentMedia = galleryCurrentMedia + 1;
+    } else {
+        galleryCurrentMedia = galleryCurrentMedia - 1;
+    }
+
+    if (galleryCurrentMedia < 0) {
+        galleryCurrentMedia = galleryMedias.length - 1;
+    } else if (galleryCurrentMedia >= galleryMedias.length) {
+        galleryCurrentMedia = 0;
+    }
+
+    $("#gallery_popup .gallery-popup-media").removeClass("active");
+    $("#gallery_popup .gallery-popup-media[data-index="+galleryCurrentMedia+"]").addClass("active");
+}
--- Widget/widgets/views/gallery_widget.php
+++ Widget/widgets/views/gallery_widget.php
@@ -1,5 +1,7 @@
 <?php
 
+use Core\Utils;
+
 echo 
     '<div id="gallery_widget_'.$gallery->id.'" class="widget widget-gallery row">'.
         '<div class="col-lg-12">'.
@@ -8,9 +10,29 @@ echo
 
 $gallery->galleriesmedias;
 if (!empty($gallery->galleriesmedias)) {
-    foreach ($gallery->galleriesmedias as $galleryMedia) {
-        if ($galleryMedia->media->type == 'image') {
-            $imageUrl = $galleryMedia->media->getUrl();
+    foreach ($gallery->galleriesmedias as $index => $galleryMedia) {
+        $media = $galleryMedia->media;
+
+        if ($media->type == 'image') {
+            $imageUrl = $media->getUrl();
+
+            $path = PUBLIC_DIR.$imageUrl;
+
+            $imageInfos = array();
+            if (file_exists($path)) {
+                $img = new \Imagick($path);
+
+                $imageInfos['width'] = $img->getImageWidth();
+                $imageInfos['height'] = $img->getImageHeight();
+                $imageInfos['size'] = Utils::bytesToHumanReadable($img->getImageLength());
+
+                $imageInfos['mime'] = $img->getImageMimeType();
+
+                $r = $img->getImageResolution();
+                $imageInfos['resolution_x'] = $r['x'];
+                $imageInfos['resolution_y'] = $r['y'];
+            }
+
         } else {
             $imageUrl = '';
         }
@@ -18,21 +40,30 @@ if (!empty($gallery->galleriesmedias)) {
         $title = 
             $galleryMedia->title != '' ?
             $galleryMedia->title : 
-                $galleryMedia->media->name != '' ?
-                $galleryMedia->media->name :
+                $media->name != '' ?
+                $media->name :
                 '';
 
         $description = 
             $galleryMedia->description != '' ?
             $galleryMedia->description : 
-                $galleryMedia->media->description != '' ?
-                $galleryMedia->media->description :
+                $media->description != '' ?
+                $media->description :
                 '';
+
+        $media = urlencode(json_encode(array(
+            'index' => $index,
+            'type' => $media->type,
+            'image' => $imageUrl,
+            'title' => $title,
+            'description' => $description,
+            'imageInfos' => $imageInfos
+        )));
 
         echo 
             '<div class="col-lg-3">'.
-                '<div class="gallery-media">'.
-                    '<img class="gallery-media-image img-fluid" src="'.$imageUrl.'" alt="'.$galleryMedia->title.'" />'.
+                '<div class="gallery-media" data-index="'.$index.'" data-media="'.$media.'">'.
+                    '<img class="gallery-media-image img-fluid" src="'.$imageUrl.'" alt="'.$title.'" data-index="'.$index.'" />'.
                     // '<div class="gallery-media-title">'.$title.'</div>'.
                     // '<div class="gallery-media-description">'.$description.'</div>'.
                 '</div>'.
